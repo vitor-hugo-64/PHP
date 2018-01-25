eea9cfbd7eb64abafe920b3da51edf8d79f57d44
Vitor Hugo Balon de Oliveira <vitorhugooliveira64@gmail.com>
2018-01-25 11:15:18 -0200

    Alterações No Arquivos Da Pasta "curso-php-completo/banco_de_dados"

--- curso-php-completo/banco_de_dados/PDO_melhor_forma/index.php
+++ curso-php-completo/banco_de_dados/PDO_melhor_forma/index.php
@@ -1,15 +1,21 @@
 <?php
-
+	
+	// DADOS DA CONEXÃO
 	$con = new PDO("mysql:dbname=curso_php; host=localhost", "root", "nunca mais10");
 
-	 $stmt = $con->prepare("SELECT * FROM usuario ORDER BY id");
+	// COMANDO DE CONSULTA DA BASE DE DADOS
+	$stmt = $con->prepare("SELECT * FROM usuario ORDER BY id");
 
-	 $stmt->execute();
+	// EXECUTA O COMANDO DE CONSULTA
+	$stmt->execute();
 
-	 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	// RETORNA TODOS OS RESULTADOS EM FORMA DE ARRAY
+	// QUANDO NÃO TIVER MAIS DADOS ELE RETORNA FALSE
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	 foreach ($result as $row) {
-	 	foreach ($row as $key => $value) {
+	// PEGA OS DADOS DO ARRAY E OS INSERE NA TELA
+	foreach ($result as $row) {
+		foreach ($row as $key => $value) {
 	 		echo "<strong>".$key.":</strong>".$value."<br>";
 	 	}
 
